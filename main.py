ebe2c8cc77876e6f08472d096b9cc01bdfd408e7
issyehrlich <75787705+issyehrlich@users.noreply.github.com>
2021-01-19 13:42:58 +0100

    add gitignore

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
 def addition(x, y):
-    return x+y
+    return x + y
+
 
 def subtraction(x, y):
-    return x-y
+    return x - y
+
 
 def main():
     print("hello world")
+
 
 if __name__ == "__main__":
     # execute only if run as a script
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
 import pytest
 import main as mn
 
+
 def test_addition():
     result = mn.addition(2, 3)
     assert result == 5
+
+
+def test_subtraction():
+    result = mn.subtraction(5.5, 3)
+    assert result == 2.5
